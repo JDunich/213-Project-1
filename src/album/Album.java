345e97c668617df40d75a6831547a11a79989bbc
Kiana <kianaperst85@gmail.com>
2021-09-17 21:01:01 -0400

    Upload files
    Create album package and upload Album.java and Genre.java.

---
+++ src/album/Album.java
@@ -0,0 +1,28 @@
+package album;
+/**
+ * 
+ */
+
+/**
+ * This class defines the data type Album
+ * 
+ * @author Kiana Perst
+ *
+ */
+public class Album {
+    private String title;
+    private String artist;
+    private Genre genre; //enum class: Classical, Country, Jazz, Pop, Unknown
+    private Date releaseDate;
+    private boolean isAvailable;
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+    }
+    
+    @Override
+    public String toString(){
+        
+    }
+}
---
+++ src/album/Genre.java
@@ -0,0 +1,13 @@
+package album;
+/**
+ * 
+ */
+
+/**
+ * @author Kiana Perst
+ *
+ */
+public enum Genre {
+    Pop, Country, Classical, Jazz, Unknown;
+
+}
