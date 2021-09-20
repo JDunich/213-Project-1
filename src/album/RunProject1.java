c224469a603929f67dd8faa10c15174101ee1b3f
Kiana <kianaperst85@gmail.com>
2021-09-19 20:48:10 -0400

    Update files
    Built constructors for the Album class and added the Date class and the RunProject1 class.

---
+++ src/album/RunProject1.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package album;
+
+/**
+ * This is a driver class to test run Project 1
+ * @author Kiana Perst
+ *
+ */
+public class RunProject1 {
+    public static void main(String[] args) {
+        new CollectionManager().run();
+    }
+
+}
